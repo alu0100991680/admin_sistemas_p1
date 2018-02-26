bef2f30505f8dbbaf668b72d931ca5a2d5b0b329
alu0100991680 <alu0100991680@ull.edu.es>
2018-02-26 21:23:54 +0100

    Creo que los tres primeros bloques estan hechos hasta aqui

--- p1.sh
+++ p1.sh
@@ -1,3 +1,6 @@
+echo "[PACKAGE QUOTA] INSTALL"
+yum install ­y quota
+yum install ­y acl
 echo "[del -r] LIMPIAR USUARIOS PREVIOS Y SUS DIRECTORIOS HOME"
 userdel -r usu1
 userdel -r usu2
@@ -13,6 +16,10 @@ groupdel Aeropuerto
 groupdel CentroComercial
 groupdel Parque
 groupdel Comun
+echo ""
+echo " ----------------- "
+echo "| MONTANDO ESQUEMA|"
+echo " ----------------- "
 echo "Practica 1: Test de comandos"
 echo "[useradd] CREAR USUARIOS"
 # Adduser es un wrapper en PERL de useradd. Adduser, es mas friendly
@@ -50,6 +57,8 @@ printf "usu3\nusu3" | sudo passwd usu3
 printf "usu4\nusu4" | sudo passwd usu4
 printf "usu5\nusu5" | sudo passwd usu5
 printf "usu6\nusu6" | sudo passwd usu6
+printf "ejec1\nejec1" | sudo passwd ejec1
+printf "ejec2\nejec2" | sudo passwd ejec2
 echo "[chmod 700. uid=xrd]"
 chmod u=rwx /home/usu1  # chmod 700
 chmod g= /home/usu1
@@ -69,6 +78,12 @@ chmod o= /home/usu5
 chmod u=rwx /home/usu6
 chmod g= /home/usu6
 chmod o= /home/usu6
+chmod u=rwx /home/ejec1
+chmod g= /home/ejec1
+chmod o= /home/ejec1
+chmod u=rwx /home/ejec2
+chmod g= /home/ejec2
+chmod o= /home/ejec2
 echo "[mkdir] CREAMOS DIRECTORIOS PARA LOS PROYECTOS"
 mkdir /export
 mkdir /export/proyectos
@@ -117,34 +132,52 @@ usermod -a -G Comun usu3
 usermod -a -G Comun usu4
 usermod -a -G Comun usu5
 usermod -a -G Comun usu6
-echo "[groups] REPARTO DE GRUPOS"
-groups root
-groups usu1
-groups usu2
-groups usu3
-groups usu4
-groups usu5
-groups usu6
-echo "[ACL+setfacl] SOLO PERMITIMOS QUE EL PROPIETARIO PUEDA ELIMINAR SUS PROPIOS CONTENIDOS DENTROS DE COMUN"
-# ACL: Proporciona un mecanismo de permisos adicionales más flexibles para los sistemas de archivos.
-# Está diseñado para ayudar con los permiso de archivos en UNIX.
-# ACL permite dar permisos para cualquier usuario o grupo a cualquier recurso del disco.
-#setfacl -R -m u::rwx /export/proyectos/Comun
-
-#setfacl -R -d -m g::rwx /export/proyectos/Comun
-#setfacl -R -d -m o::rw- /export/proyectos/Comun
-
-#setfacl -Rdm g:Comun:rw- /export/proyectos/Comun
-#setfacl -Rm o::rw- /export/proyectos/Comun # Others no tiene sentido indicar el grupo
-echo "[ACL+getfacl] MOSTRAR ESTADO ACL DEL DIRECTORIO COMUN"
-getfacl /export/proyectos/Comun
-echo "[groupadd] CREAMOS LOS GRUPOS PARA LOS EJECUTIVOS. CON PERMISOS MAS RESTRICTIVOS"
-groupadd EjecAeropuerto
-groupadd EjecParque
-groupadd EjecCentroComercial
-echo "[usermod] ASOCIAMOS LOS EJECUTIVOS A LOS NUEVOS GRUPOS"
-usermod -a -G EjecAeropuerto ejec1
-usermod -a -G EjecAeropuerto ejec2
-usermod -a -G EjecParque ejec1
-usermod -a -G EjecCentroComercial ejec2
+#echo "[groups] REPARTO DE GRUPOS"
+#groups root
+#groups usu1
+#groups usu2
+#groups usu3
+#groups usu4
+#groups usu5
+#groups usu6
+echo "[fstab+mount] CREAMOS LAS CUOTAS DE ESPACIO"
+echo "··NOTA: RECUERDA EDITAR /etc/fstab añadiendo -usrquota- dentras de default en la partición /home"
+echo "··      /dev/mapper/sistema-home /home    ext4    defaults, usrquota   1   2"
+mount -o remount /home
+echo "··Desactivando servicio previo"
+quotaoff /home
+echo "··Checking..."
+quotacheck -ugmv /home
+echo "··¿Existe config-file?"
+ls /home/aquota.user
+echo "··Activando el servicio"
+quotaon -ugv /home
+echo "··Añadimos los limites a cada usuario"
+setquota -u usu1 51200 60000 0 0 /home
+setquota -u usu2 51200 60000 0 0 /home
+setquota -u usu3 51200 60000 0 0 /home
+setquota -u usu4 51200 60000 0 0 /home
+setquota -u usu5 51200 60000 0 0 /home
+setquota -u usu6 51200 60000 0 0 /home
+echo "··Establecimiento de tiempo de gracia"
+echo "··NOTA: Editar mediante edquota -t para globales. Ejemplo 2days 2days"
+echo "        Editar mediante edquota -u <usuario> -t para usuario concreto. Ejemplo 2days 2days"
+echo "        /dev/mapper/sistema-home     2days    2days"
+echo "[chmod+sticky] SOLO PERMITIMOS QUE EL PROPIETARIO PUEDA ELIMINAR SUS PROPIOS CONTENIDOS DENTROS DE COMUN"
+chmod o+t /export/proyectos/Comun
+echo "[setGID] ????"
+chmod 2770 /export/proyectos/Aeropuerto
+chmod 2770 /export/proyectos/CentroComercial
+chmod 2770 /export/proyectos/Parque
+#echo "[ACL+getfacl] MOSTRAR ESTADO ACL DEL DIRECTORIO COMUN"
+#getfacl /export/proyectos/Comun
+#echo "[groupadd] CREAMOS LOS GRUPOS PARA LOS EJECUTIVOS. CON PERMISOS MAS RESTRICTIVOS"
+#groupadd EjecAeropuerto
+#groupadd EjecParque
+#groupadd EjecCentroComercial
+#echo "[usermod] ASOCIAMOS LOS EJECUTIVOS A LOS NUEVOS GRUPOS"
+#usermod -a -G EjecAeropuerto ejec1
+#usermod -a -G EjecAeropuerto ejec2
+#usermod -a -G EjecParque ejec1
+#usermod -a -G EjecCentroComercial ejec2
 

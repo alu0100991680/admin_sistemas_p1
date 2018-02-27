646fce6a66d21535dd6941dd80c2bc62ce3c6bcc
alu0100991680 <alu0100991680@ull.edu.es>
2018-02-27 15:09:38 +0100

    Mejoras y correcciones de errores

--- p1.sh
+++ p1.sh
@@ -1,35 +1,71 @@
+echo " ------------ "
+echo "| PRÁCTICA 1 |"
+echo " ------------ "
+echo ""
 echo "[PACKAGE QUOTA] INSTALL"
 yum install ­y quota
 yum install ­y acl
 echo "[del -r] LIMPIAR USUARIOS PREVIOS Y SUS DIRECTORIOS HOME"
-userdel -r usu1
-userdel -r usu2
-userdel -r usu3
-userdel -r usu4
-userdel -r usu5
-userdel -r ejec1
-userdel -r ejec2
+userdel -r usu1 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r usu2 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r usu3 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r usu4 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r usu5 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r usu6 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r ejec1 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+userdel -r ejec2 # Eliminará todo lo que pueda menos el directorio HOME ya que más abajo cambiamos el propietario a root
+echo "··Eliminamos directorios personales que userdel -r no puede por permisos"
+rm -rf /home/usu1 # Ahora eliminamos lo que en el paso anterior no pudimos
+rm -rf /home/usu2
+rm -rf /home/usu3
+rm -rf /home/usu4
+rm -rf /home/usu5
+rm -rf /home/usu6
+rm -rf /home/ejec1
+rm -rf /home/ejec2
 echo "[rm -rf /export as root] ELINAMOS TODO LO QUE TIENE QUE VER CON LOS PROYECTOS"
-rm -rf /export
+rm -rf /export # Eliminamos todo el arbol de la práctica /export/...
 echo "[groupdel] ELIMINAMOS LOS GRUPOS"
-groupdel Aeropuerto
+groupdel Aeropuerto # Eliminamos todos los grupos creados para la práctica
 groupdel CentroComercial
 groupdel Parque
+groupdel Eaeropuerto
+groupdel Ecentrocomercial
+groupdel Eparque
 groupdel Comun
 echo ""
-echo " ----------------- "
-echo "| MONTANDO ESQUEMA|"
-echo " ----------------- "
+echo " ------------------ "
+echo "| MONTANDO ESQUEMA |"
+echo " ------------------ "
 echo "Practica 1: Test de comandos"
-echo "[useradd] CREAR USUARIOS"
+echo "[useradd+chown+chmod] CREAR USUARIOS BLOQUEANDO QUE PUEDAN CAMBIAR SUS PROPIOS PERMISOS"
 # Adduser es un wrapper en PERL de useradd. Adduser, es mas friendly
 useradd -m -d /home/usu1 usu1
 useradd -m -d /home/usu2 usu2
 useradd -m -d /home/usu3 usu3
 useradd -m -d /home/usu4 usu4
 useradd -m -d /home/usu5 usu5
+useradd -m -d /home/usu6 usu6
 useradd -m -d /home/ejec1 ejec1
 useradd -m -d /home/ejec2 ejec2
+echo "··Ahora bloqueamos que el usurio por si mismo no pueda cambiar sus permisos"
+chown root:usu1 /home/usu1
+chown root:usu2 /home/usu2
+chown root:usu3 /home/usu3
+chown root:usu4 /home/usu4
+chown root:usu5 /home/usu5
+chown root:usu6 /home/usu6
+chown root:ejec1 /home/ejec1
+chown root:ejec2 /home/ejec2
+echo "··Cambiamos los permios para que si se pueda acceder"
+chmod 070 /home/usu1
+chmod 070 /home/usu2
+chmod 070 /home/usu3
+chmod 070 /home/usu4
+chmod 070 /home/usu5
+chmod 070 /home/usu6
+chmod 070 /home/ejec1
+chmod 070 /home/ejec2
 echo "[chage] CADUCIDAD EN PASSWORD PARA 3 MESES"
 # La herramienta chage modifica el número de días entre
 # cambios de contraseña y la fecha del último cambio de contraseña.
@@ -46,6 +82,7 @@ chage -M 90 -I 2 -W 1 usu4
 chage -M 90 -I 2 -W 1 usu5
 chage -M 90 -I 2 -W 1 usu6
 echo "[chage -l] LISTAR LAS CONDICIONES DE CUENTA"
+echo "··Ejemplo"
 chage -l usu1
 echo "[passwd] CAMBIAR PASSWORD A CADA USUARIO"
 # En la practica manual usamos el comando passwd
@@ -59,94 +96,16 @@ printf "usu5\nusu5" | sudo passwd usu5
 printf "usu6\nusu6" | sudo passwd usu6
 printf "ejec1\nejec1" | sudo passwd ejec1
 printf "ejec2\nejec2" | sudo passwd ejec2
-echo "[chmod 700. uid=xrd]"
-chmod u=rwx /home/usu1  # chmod 700
-chmod g= /home/usu1
-chmod o= /home/usu1
-chmod u=rwx /home/usu2
-chmod g= /home/usu2
-chmod o= /home/usu2
-chmod u=rwx /home/usu3
-chmod g= /home/usu3
-chmod o= /home/usu3
-chmod u=rwx /home/usu4
-chmod g= /home/usu4
-chmod o= /home/usu4
-chmod u=rwx /home/usu5
-chmod g= /home/usu5
-chmod o= /home/usu5
-chmod u=rwx /home/usu6
-chmod g= /home/usu6
-chmod o= /home/usu6
-chmod u=rwx /home/ejec1
-chmod g= /home/ejec1
-chmod o= /home/ejec1
-chmod u=rwx /home/ejec2
-chmod g= /home/ejec2
-chmod o= /home/ejec2
-echo "[mkdir] CREAMOS DIRECTORIOS PARA LOS PROYECTOS"
-mkdir /export
-mkdir /export/proyectos
-mkdir /export/proyectos/Aeropuerto
-mkdir /export/proyectos/CentroComercial
-mkdir /export/proyectos/Parque
-mkdir /export/proyectos/Comun
-echo "[groupadd] CREAMOS LOS GRUPOS PARA CADA PROYECTO"
-groupadd Aeropuerto
-groupadd CentroComercial
-groupadd Parque
-groupadd Comun
-echo "[chgrp] ASIGNAMOS/CAMBIAMOS LOS GRUPOS A LOS DIRECTORIOS QUE CREAMOS PARA CADA PROYECTO"
-chgrp Aeropuerto /export/proyectos/Aeropuerto
-chgrp CentroComercial /export/proyectos/CentroComercial
-chgrp Parque /export/proyectos/Parque
-chgrp Comun /export/proyectos/Comun
-echo "[usermod] ASIGNAMOS A CADA USUARIO SU GRUPO"
-usermod -a -G Aeropuerto usu2
-usermod -a -G Aeropuerto usu3
-usermod -a -G Aeropuerto usu4
-usermod -a -G Aeropuerto usu5
-usermod -a -G CentroComercial usu1
-usermod -a -G CentroComercial usu3
-usermod -a -G CentroComercial usu4
-usermod -a -G CentroComercial usu5
-usermod -a -G Parque usu5
-usermod -a -G Parque usu6
-echo "[chmod] LIMITAMOS EL ACCESO ENTRE GRUPOS PARA ESOS DIRECTORIOS"
-chmod u=rwx /export/proyectos/Aeropuerto
-chmod g=rwx /export/proyectos/Aeropuerto
-chmod o= /export/proyectos/Aeropuerto
-chmod u=rwx /export/proyectos/CentroComercial
-chmod g=rwx /export/proyectos/CentroComercial
-chmod o= /export/proyectos/CentroComercial
-chmod u=rwx /export/proyectos/Parque
-chmod g=rwx /export/proyectos/Parque
-chmod o= /export/proyectos/Parque
-chmod u=rwx /export/proyectos/Comun
-chmod g=rwx /export/proyectos/Comun
-chmod o= /export/proyectos/Comun
-echo "[usermod] ASIGNAMOS LOS USUARIOS A LA CARPETA COMUN"
-usermod -a -G Comun usu1
-usermod -a -G Comun usu2
-usermod -a -G Comun usu3
-usermod -a -G Comun usu4
-usermod -a -G Comun usu5
-usermod -a -G Comun usu6
-#echo "[groups] REPARTO DE GRUPOS"
-#groups root
-#groups usu1
-#groups usu2
-#groups usu3
-#groups usu4
-#groups usu5
-#groups usu6
+echo " -------------------------------------------------------------- "
+echo "| QUOTAS DE DISCO. SOLO APLICABLE EN LA IMAGEN CENTOS7 DE IAAS |"
+echo " -------------------------------------------------------------- "
 echo "[fstab+mount] CREAMOS LAS CUOTAS DE ESPACIO"
 echo "··NOTA: RECUERDA EDITAR /etc/fstab añadiendo -usrquota- dentras de default en la partición /home"
 echo "··      /dev/mapper/sistema-home /home    ext4    defaults, usrquota   1   2"
 mount -o remount /home
 echo "··Desactivando servicio previo"
 quotaoff /home
-echo "··Checking..."
+echo "··Creamos el sistema de archivos"
 quotacheck -ugmv /home
 echo "··¿Existe config-file?"
 ls /home/aquota.user
@@ -163,22 +122,87 @@ echo "··Establecimiento de tiempo de gracia"
 echo "··NOTA: Editar mediante edquota -t para globales. Ejemplo 2days 2days"
 echo "        Editar mediante edquota -u <usuario> -t para usuario concreto. Ejemplo 2days 2days"
 echo "        /dev/mapper/sistema-home     2days    2days"
-echo "[chmod+sticky] SOLO PERMITIMOS QUE EL PROPIETARIO PUEDA ELIMINAR SUS PROPIOS CONTENIDOS DENTROS DE COMUN"
-chmod o+t /export/proyectos/Comun
+echo " -------------------------------- "
+echo "| CREANDO ESPACIO PARA PROYECTOS |"
+echo " -------------------------------- "
+echo "[mkdir] CREAMOS DIRECTORIOS PARA LOS PROYECTOS"
+mkdir /export
+mkdir /export/proyectos
+mkdir /export/proyectos/Aeropuerto
+mkdir /export/proyectos/CentroComercial
+mkdir /export/proyectos/Parque
+mkdir /export/proyectos/Comun
+echo "[groupadd] CREAMOS LOS GRUPOS PARA CADA PROYECTO"
+groupadd Aeropuerto
+groupadd CentroComercial
+groupadd Parque
+groupadd Comun
+echo "[groupadd] CREAMOS LOS GRUPOS PARA LOS EJECUTIVOS"
+groupadd Eaeropuerto
+groupadd Ecentrocomercial
+groupadd Eparque
+echo "[chgrp] ASIGNAMOS/CAMBIAMOS LOS GRUPOS A LOS DIRECTORIOS QUE CREAMOS PARA CADA PROYECTO"
+chgrp Aeropuerto /export/proyectos/Aeropuerto
+chgrp CentroComercial /export/proyectos/CentroComercial
+chgrp Parque /export/proyectos/Parque
+chgrp Comun /export/proyectos/Comun
+echo "[usermod] ASIGNAMOS A CADA USUARIO SU GRUPO"
+usermod -a -G Aeropuerto usu2
+usermod -a -G Aeropuerto usu3
+usermod -a -G Aeropuerto usu4
+usermod -a -G Aeropuerto usu5
+usermod -a -G CentroComercial usu1
+usermod -a -G CentroComercial usu3
+usermod -a -G CentroComercial usu4
+usermod -a -G CentroComercial usu5
+usermod -a -G Parque usu5
+usermod -a -G Parque usu6
+echo "[usermod] ASIGNAMOS A CADA EJECUTIVO SU GRUPO"
+usermod -a -G Eaeropuerto ejec1
+usermod -a -G Eaeropuerto ejec2
+usermod -a -G Ecentrocomercial ejec2
+usermod -a -G Eparque ejec1
+echo "[usermod] ASIGNAMOS LOS USUARIOS A LA CARPETA COMUN"
+usermod -a -G Comun usu1
+usermod -a -G Comun usu2
+usermod -a -G Comun usu3
+usermod -a -G Comun usu4
+usermod -a -G Comun usu5
+usermod -a -G Comun usu6
 echo "[setGID] ????"
+#chmod g+w /export/proyectos/Aeropuerto
+#chmod g+w /export/proyectos/CentroComercial
+#chmod g+w /export/proyectos/Parque
+#chmod o+r,o+x /export/proyectos/Aeropuerto
+#chmod o+r,o+x /export/proyectos/CentroComercial
+#chmod o+r,o+x /export/proyectos/Parque
 chmod 2770 /export/proyectos/Aeropuerto
 chmod 2770 /export/proyectos/CentroComercial
 chmod 2770 /export/proyectos/Parque
+echo "[chmod+sticky] SOLO PERMITIMOS QUE EL PROPIETARIO PUEDA ELIMINAR SUS PROPIOS CONTENIDOS DENTROS DE COMUN"
+chmod 770 /export/proyectos/Comun # Cuidado con el orden en que se definen los permisos en conjunto con StickyBit
+chmod o+t /export/proyectos/Comun
+#chmod 2770 /export/proyectos/Comun
+echo " ------------ "
+echo "| EJECUTIVOS |"
+echo " ------------ "
 echo "[ACL+setfacl] LIMPIAMOS ACL"
-setfacl -b -k -R /export/proyectos/Areopuerto
+setfacl -b -k -R /export/proyectos/Aeropuerto
 setfacl -b -k -R /export/proyectos/CentroComercial
 setfacl -b -k -R /export/proyectos/Parque
 echo "[ACL+setfacl] ASOCIAMOS ACL CON LOS NUEVOS GRUPOS PARA EJECUTIVOS"
-setfacl -m g:EjectutivoAreopuerto:---­/export/proyectos/Areopuerto
-setfacl -m g:EjectutivoCentroComercial:---­/export/proyectos/CentroComercial
-setfacl -m g:EjectutivoParque:---­/export/proyectos/Parque
+setfacl -m g:EjecutivoAeropuerto:x­/export/proyectos/Aeropuerto
+setfacl -m g:EjecutivoCentroComercial:x­/export/proyectos/CentroComercial
+setfacl -m g:EjecutivoParque:x­/export/proyectos/Parque
 echo "[cp ls] COPIAMOS ls PARA CADA UNO DE LOS DELEGADOS"
-cp /bin/ls /bin/ls_areopuerto
-cp /bin/ls /bin/ls_centrocomercial
-cp /bin/ls /bin/ls_parque
-echo "[]CAMBIAMOS LOS GRUPOS PARA ls"
+cp /bin/ls /usr/local/bin/ls_aeropuerto
+cp /bin/ls /usr/local/bin/ls_centrocomercial
+cp /bin/ls /usr/local/bin/ls_parque
+echo "[chgrp] CAMBIAMOS LOS GRUPOS PARA ls"
+chgrp Eaeropuerto /usr/local/bin/ls_areopuerto
+chgrp Ecentrocomercial /usr/local/bin/ls_centrocomercial
+chgrp Eparque /usr/local/bin/ls_parque
+echo "[ACL+ls] AÑADIMOS LOS PERMISOS NECESARIOS A LOS ls por medio de ACL"
+setfacl -m g:Eaeropuerto:x /usr/local/bin/ls_aeropuerto
+setfacl -m g:Ecentrocomercial:x /usr/local/bin/ls_centrocomercial
+setfacl -m g:Eparque:x /usr/local/bin/ls_parque
